755ff7ec97a5f4498a57855d1058722d080dcc50
codewithashish1991 <wtit786@gmail.com>
2026-02-07 16:05:02 +0530

    update table

--- backend/models/attendance.py
+++ backend/models/attendance.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String, Date, ForeignKey
 from database import Base
+from sqlalchemy.orm import relationship
 
 class Attendance(Base):
     __tablename__ = "attendance"
--- backend/models/employee.py
+++ backend/models/employee.py
@@ -1,12 +1,13 @@
 from sqlalchemy import Column, String
 from database import Base
+from sqlalchemy.orm import relationship
 
 class Employee(Base):
     __tablename__ = "employees"
 
     employee_id = Column(String, primary_key=True)
     name = Column(String, nullable=False)
-    email = Column(String, nullable=False)
+    email = Column(String, nullable=False, unique=True)
     department = Column(String, nullable=False)
 
     attendance = relationship(
